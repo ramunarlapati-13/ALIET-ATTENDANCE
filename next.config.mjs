274a09a0d4205ef7bb452fe85d503f95986c13ea
ramunarlapati-13 <ramunarlapati@gmail.com>
2026-02-08 01:09:48 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: '/ALIETake',
+    basePath: '/ALIET-ATTENDANCE',
     images: {
         unoptimized: true,
         domains: ['firebasestorage.googleapis.com'],
